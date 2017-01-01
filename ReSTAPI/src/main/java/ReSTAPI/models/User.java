60e76351f44d21a5fcf8c1a16cb2b925cfec6d92
Glenn-Faison <glennfaison@gmail.com>
2017-01-01 12:48:07 +0100

    Initial commit for 2017.

--- ReSTAPI/src/main/java/ReSTAPI/models/User.java
+++ ReSTAPI/src/main/java/ReSTAPI/models/User.java
@@ -24,7 +24,7 @@ public class User {
     @Column(nullable = false, unique = true)
     private String username;
 
-    @OneToMany(orphanRemoval = true, cascade = CascadeType.ALL, targetEntity = Task.class)
+    @OneToMany(orphanRemoval = true, cascade = CascadeType.ALL)
     private List<Task> taskList;
 
     @NotNull
